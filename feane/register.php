474b8fe47d3a4abd4db3b0c8c3a260da31fdbd47
christine chen <yaa10605@gmail.com>
2022-04-27 14:24:06 +0800

    小修修

--- feane/register.php
+++ feane/register.php
@@ -271,9 +271,9 @@ if(isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["phone"]) && 
             <a href="index.php" class="footer-logo">
               方禾食呂
             </a>
-            <p>
+            <h5 style="color:aliceblue">
             健康飲食好夥伴
-            </p>
+            </h5>
             <div class="footer_social">
               <a href="https://www.facebook.com/storyboxtw/about/?ref=page_internal">
                 <i class="fa fa-facebook" aria-hidden="true"></i>
