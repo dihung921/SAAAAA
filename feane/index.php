27945508438c4433eb1bb92783435092f4f297a8
yizzz002 <yvettewyz@gmail.com>
2022-05-03 14:06:40 +0800

    0503

--- feane/index.php
+++ feane/index.php
@@ -357,7 +357,7 @@ if(isset($_POST["way"])){
         <li class="active" data-filter="*">全部商品</li>
 
         <a  style="text-decoration: none; color:black;" href="#classic"><li>經典餐盒</li></a>
-        <a style="text-decoration: none; color:black;" href="#roll"><sli>輕食捲捲</li></a>
+        <a style="text-decoration: none; color:black;" href="#roll"><li>輕食捲捲</li></a>
         <a style="text-decoration: none; color:black;" href="#salad"><li>沙拉水果盒</li></a>
         <a style="text-decoration: none; color:black;" href="#main"><li>主食單品</li></a>
         <a style="text-decoration: none; color:black;" href="#other"><li>其他單品</li></a>
