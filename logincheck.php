97c3f19aa8e10fe762f0e812fc4fa96e24a9b936
yizzz002 <104059984+yizzz002@users.noreply.github.com>
2022-05-06 23:07:03 +0800

    Add files via upload

---
+++ logincheck.php
@@ -0,0 +1,33 @@
+<?php
+   session_start();
+
+   $link = mysqli_connect("localhost", "root");
+   mysqli_select_db($link,"sa");
+   
+
+//                         
+//   if(isset($_SESSION["account_name"])){
+//           header("Location:index.php");
+//       }
+   if(isset($_POST["phone"])&& isset($_POST["password"])){
+       $phone = $_POST["phone"];
+       $password = $_POST["password"];
+       $sql="select * from member where phone ='$phone'";
+       $rs=mysqli_query($link,$sql);
+      
+   if($row = mysqli_fetch_array($rs)){
+       if($row["password"] == $_POST["password"]){
+           $_SESSION["member_name"]= $row["name"];
+           $_SESSION["member_email"]= $row["email"];
+           $_SESSION["member_phone"] = $row["phone"];
+           $_SESSION["member_password"]= $row["password"];
+           header("location:index.php");
+       } 
+       else{
+        echo "<script>{window.alert('密碼錯誤！請再試一次'); location.href='login.php'}</script>";
+   }
+   }
+   else{
+    echo "<script>{window.alert('此手機號碼尚未註冊！請先註冊帳號'); location.href='register.php'}</script>";
+   }
+}
---
+++ profile.php
@@ -0,0 +1,410 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <link rel="shortcut icon" href="images/favicon.png" type="">
+
+  <title> 方禾食呂 </title>
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+  <!--owl slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+  <!-- nice select  -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/css/nice-select.min.css" integrity="sha512-CruCP+TD3yXzlvvijET8wV5WxxEh5H8P4cmz0RFbKK6FlZ2sYl3AEsKlLPHbniXKSrDdFewhbmBK5skbdsASbQ==" crossorigin="anonymous" />
+  <!-- font awesome style -->
+  <link href="css/font-awesome.min.css" rel="stylesheet" />
+
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+
+</head>
+
+<body class="sub_page">
+
+  <div class="hero_area">
+    <div class="bg-box">
+      <img src="images/ll4.png" alt="">
+    </div>
+    <!-- header section strats -->
+    <header class="header_section">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand" href="index.php">
+            <span>
+              方禾食呂
+            </span>
+          </a>
+
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class=""> </span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav  mx-auto ">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">訂餐首頁</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.php">關於方禾</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="seat.php">店內座位狀況</a>
+              </li>
+            </ul>
+            <div class="user_option">
+            <?php
+            if ($_SESSION["member_name"]){
+              echo "<a href='profile.php' class='user_link'>
+              <i class='fa fa-user' aria-hidden='true'></i>
+            </a>";
+            }
+            ?>
+              <a class="cart_link" href="cart.php">
+                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 456.029 456.029" style="enable-background:new 0 0 456.029 456.029;" xml:space="preserve">
+                  <g>
+                    <g>
+                      <path d="M345.6,338.862c-29.184,0-53.248,23.552-53.248,53.248c0,29.184,23.552,53.248,53.248,53.248
+                   c29.184,0,53.248-23.552,53.248-53.248C398.336,362.926,374.784,338.862,345.6,338.862z" />
+                    </g>
+                  </g>
+                  <g>
+                    <g>
+                      <path d="M439.296,84.91c-1.024,0-2.56-0.512-4.096-0.512H112.64l-5.12-34.304C104.448,27.566,84.992,10.67,61.952,10.67H20.48
+                   C9.216,10.67,0,19.886,0,31.15c0,11.264,9.216,20.48,20.48,20.48h41.472c2.56,0,4.608,2.048,5.12,4.608l31.744,216.064
+                   c4.096,27.136,27.648,47.616,55.296,47.616h212.992c26.624,0,49.664-18.944,55.296-45.056l33.28-166.4
+                   C457.728,97.71,450.56,86.958,439.296,84.91z" />
+                    </g>
+                  </g>
+                  <g>
+                    <g>
+                      <path d="M215.04,389.55c-1.024-28.16-24.576-50.688-52.736-50.688c-29.696,1.536-52.224,26.112-51.2,55.296
+                   c1.024,28.16,24.064,50.688,52.224,50.688h1.024C193.536,443.31,216.576,418.734,215.04,389.55z" />
+                    </g>
+                  </g>
+                  <g>
+                  </g>
+                  <g>
+                  </g>
+                  <g>
+                  </g>
+                  <g>
+                  </g>
+                  <g>
+                  </g>
+                  <g>
+                  </g>
+                  <g>
+                  </g>
+                  <g>
+                  </g>
+                  <g>
+                  </g>
+                  <g>
+                  </g>
+                  <g>
+                  </g>
+                  <g>
+                  </g>
+                  <g>
+                  </g>
+                  <g>
+                  </g>
+                  <g>
+                  </g>
+                </svg>
+              </a>
+              <form action="logout.php" method="post">
+              <?php
+              if ($_SESSION["member_name"]){
+                
+                  ?>
+                  <a style="color: white"><?php echo $_SESSION["member_name"]; ?></a>
+                  <?php
+                echo "<button class='order_online'>登出</button>";
+              }
+              else{
+                echo "<a href='login.php' class='order_online'>
+                登入
+              </a>
+              <a href='register.php' class='order_online'>
+                註冊
+              </a>";
+              }
+              ?>
+            </form>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- end header section -->
+  </div>
+
+  
+
+  <!-- profile section -->
+  <br>
+  <br>
+
+
+  <div class="container">
+    <div class="main-body">
+    
+          
+    
+          <div class="row gutters-sm">
+            <div class="col-md-4 mb-3">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex flex-column align-items-center text-center">
+                    <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
+                    <div class="mt-3">
+                      <h4><?php echo $_SESSION["member_name"] ?></h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-8">
+              <div class="card mb-3">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">姓名</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                    <?php echo $_SESSION["member_name"] ?>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Email</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                    <?php echo $_SESSION["member_email"] ?>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">手機號碼</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                    <?php echo $_SESSION["member_phone"] ?>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">密碼</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                    <?php echo $_SESSION["member_password"] ?>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <a class="btn btn-warning " target="__blank" href="https://www.bootdey.com/snippets/view/profile-edit-data-and-skills">編輯</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            
+
+                
+                    <div class="tab-pane  fade  active show" id="orders" role="tabpanel" aria-labelledby="orders-tab">
+                        <h4 class="font-weight-bold mt-0 mb-4">訂單記錄</h4>
+                        <div class="bg-white card mb-4 order-list shadow-sm">
+                            <div class="gold-members p-4">
+                                <div class="media">
+                                    <div class="media-body">
+                                        <a href="#">
+                                            <span class="float-right text-warning">訂單狀態：準備中<i class="icofont-check-circled text-success"></i></span>
+                                        </a>
+                                        <p class="text-gray mb-3"><i class="icofont-list"></i> 訂單編號 #25102589748 <i class="icofont-clock-time ml-2"></i>成立時間 2022/4/24 6:26 PM</p>
+                                        <p class="text-dark">日式薑燒豬（地瓜,芥末醬）x 1
+                                        </p>
+                                        <p class="text-dark">泰式打拋豬（紫米飯,沙茶醬）x 1
+                                        </p>
+                                        <hr>
+                                        <div class="float-right">
+                                            <a class="btn btn-sm btn-outline-warning" href="#"><i class="icofont-headphone-alt"></i> 給予回饋</a>
+                                            <a class="btn btn-sm btn-warning" href="#"><i class="icofont-refresh"></i> 再買一次</a>
+                                        </div>
+                                        <p class="mb-0 text-black text-warning pt-2"><span class="text-black font-weight-bold"> 訂單總金額:</span> $300
+                                        </p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="bg-white card mb-4 order-list shadow-sm">
+                            <div class="gold-members p-4">
+                                <div class="media">
+                                    <div class="media-body">
+                                        <a href="#">
+                                            <span class="float-right text-warning">訂單狀態：已完成<i class="icofont-check-circled text-success"></i></span>
+                                        </a>
+                                        <p class="text-gray mb-3"><i class="icofont-list"></i> 訂單編號 #25102589535 <i class="icofont-clock-time ml-2"></i>成立時間 2022/4/23 7:52 PM</p>
+                                        <p class="text-dark">韓式風味牛（糙米飯,豆瓣醬）x 1
+                                        </p>
+                                        <hr>
+                                        <div class="float-right">
+                                            <a class="btn btn-sm btn-outline-warning" href="#"><i class="icofont-headphone-alt"></i> 給予回饋</a>
+                                            <a class="btn btn-sm btn-warning" href="#"><i class="icofont-refresh"></i> 再買一次</a>
+                                        </div>
+                                        <p class="mb-0 text-black text-warning pt-2"><span class="text-black font-weight-bold"> 訂單總金額:</span> $150
+                                        </p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="bg-white card mb-4 order-list shadow-sm">
+                            <div class="gold-members p-4">
+                                <div class="media">
+                                    <div class="media-body">
+                                        <a href="#">
+                                            <span class="float-right text-warning">訂單狀態：已完成<i class="icofont-check-circled text-success"></i></span>
+                                        </a>
+                                        <p class="text-gray mb-3"><i class="icofont-list"></i> 訂單編號 #25102589084 <i class="icofont-clock-time ml-2"></i>成立時間 2022/4/22 12:45 PM</p>
+                                        <p class="text-dark">普羅旺斯鱈魚（糙米飯,豆瓣醬）x 1
+                                        </p>
+                                        <p class="text-dark">檸檬椒鹽雞胸（白飯,沙拉醬）x 1
+                                        </p>
+                                        <p class="text-dark">陽光沙拉盒 x 1
+                                        </p>
+                                        <hr>
+                                        <div class="float-right">
+                                            <a class="btn btn-sm btn-outline-warning" href="#"><i class="icofont-headphone-alt"></i> 給予回饋</a>
+                                            <a class="btn btn-sm btn-warning" href="#"><i class="icofont-refresh"></i> 再買一次</a>
+                                        </div>
+                                        <p class="mb-0 text-black text-warning pt-2"><span class="text-black font-weight-bold"> 訂單總金額:</span> $450
+                                        </p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+        
+              
+            </div>
+          </div>
+
+        </div>
+    </div>
+    <br>
+    <br>
+  <!-- end profile section -->
+
+  <!-- footer section -->
+  <footer class="footer_section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 footer-col">
+          <div class="footer_contact">
+            <h4>
+              聯絡我們
+            </h4>
+            <div class="contact_link_box">
+              <a href="https://www.google.com/maps/place/%E6%96%B9%E7%A6%BE%E9%A3%9F%E5%91%82/@25.03403,121.430541,15z/data=!4m2!3m1!1s0x0:0xe3a4beb2b893c821?sa=X&ved=2ahUKEwibkauQl6f3AhV1yosBHaD9AY4Q_BJ6BAhgEAU">
+                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                <span>
+                242新北市新莊區中正路514巷53弄39號
+                </span>
+              </a>
+                <i class="fa fa-phone" aria-hidden="true"></i>
+                <span>
+                  Call +02 2908-1397
+                </span>
+                <i class="fa fa-envelope" aria-hidden="true"></i>
+                <span>
+                storyboxtw@gmail.com
+                </span>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 footer-col">
+          <div class="footer_detail">
+            <a href="index.php" class="footer-logo">
+              方禾食呂
+            </a>
+            <h5 style="color:aliceblue">
+            健康飲食好夥伴
+            </h5>
+            <div class="footer_social">
+              <a href="https://www.facebook.com/storyboxtw/about/?ref=page_internal">
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+              </a>
+              <a href="https://www.instagram.com/storyboxtw/">
+                <i class="fa fa-instagram" aria-hidden="true"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 footer-col">
+          <h4>
+            營業時間
+          </h4>
+          <p>
+            星期一～日
+          </p>
+          <p>
+            10:00 AM ~ 19:00 PM
+          </p>
+        </div>
+      </div>
+      <div class="footer-warning">
+        <p>
+          &copy; <span id="displayYear"></span> All Rights Reserved By
+          <a href="https://html.design/">SA05</a><br><br>
+        </p>
+      </div>
+    </div>
+  </footer>
+  <!-- footer section -->
+
+  <!-- jQery -->
+  <script src="js/jquery-3.4.1.min.js"></script>
+  <!-- popper js -->
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+  </script>
+  <!-- bootstrap js -->
+  <script src="js/bootstrap.js"></script>
+  <!-- owl slider -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+  </script>
+  <!-- isotope js -->
+  <script src="https://unpkg.com/isotope-layout@3.0.4/dist/isotope.pkgd.min.js"></script>
+  <!-- nice select -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/js/jquery.nice-select.min.js"></script>
+  <!-- custom js -->
+  <script src="js/custom.js"></script>
+  <!-- Google Map -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap">
+  </script>
+  <!-- End Google Map -->
+
+</body>
+
+</html>
