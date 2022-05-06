12ce92d56312f195b3bec3978ec267138f73b3d7
yizzz002 <104059984+yizzz002@users.noreply.github.com>
2022-05-07 03:20:30 +0800

    Delete feane directory

--- feane/changeway.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if(isset($_SESSION["way"])){
-    unset($_SESSION["way"]);
-    header("Location:index.php" );
-}
-?>
--- feane/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-session_destroy();
-echo "<script>{window.alert('已登出！'); location.href='index.php'}</script>";
-?>
