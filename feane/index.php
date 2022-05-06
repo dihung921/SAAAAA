b98a328f004a8e35961cb8b13e0c759dd023116b
yizzz002 <yvettewyz@gmail.com>
2022-05-07 03:23:21 +0800

    暫存

--- feane/index.php
+++ feane/index.php
@@ -114,7 +114,7 @@ if(isset($_POST["way"])){
     </div>
 </div>";
 }
-?>}
+?>
   <div class="hero_area">
     <div class="bg-box">
       <img src="images/ll4.png" alt="">
@@ -3857,8 +3857,6 @@ if(isset($_POST["way"])){
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script> 
 
-
-rap.js"></script>
 
   <!-- owl slider -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
