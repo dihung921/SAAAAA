1c99c8d04ae5e2936d984771aa5d572bac23537d
kefangggg <kefang0801@gmail.com>
2022-05-09 20:06:55 +0800

    email主鍵

--- feane/seat.php
+++ feane/seat.php
@@ -1,4 +1,20 @@
+<?php
+session_start();
+$link=mysqli_connect("localhost","root","12345678","sa");
 
+
+if(isset($_POST["seatnum"])){
+  $seatnum = $_POST["seatnum"];
+  $sql ="select seat from `way` where seat = '$seatnum'";
+
+  $rs=mysqli_query($link,$sql);
+  if($rs != NULL){
+
+    $_SESSION["seatnum"] = $seatnum;
+    echo "$seatnum";
+  }
+}
+?>
 
 <!DOCTYPE html>
 <html>
@@ -185,43 +201,15 @@
 
   <!-- book section -->
   <div class="cseat">
- 
-      <label>選擇位置:</label>
-      <select class="cselect" id="seat">
-        <option value="1">座位1</option>
-        <option value="2">座位2</option>
-        <option value="3">座位3</option>
-        <option value="4">座位4</option>
-        <option value="5">座位5</option>
-        <option value="6">座位6</option>
-        <option value="7">座位7</option>
-        <option value="8">座位8</option>
-        <option value="9">座位9</option>
-        <option value="10">座位10</option>
-        <option value="11">座位11</option>
-        <option value="12">座位12</option>
-        <option value="13">座位13</option>
-        <option value="14">座位14</option>
-        <option value="15">座位15</option>
-        <option value="16">座位16</option>
-        
-      </select>
 <div class="movie-container">
     <ul class="showcase">
       <li>
         <div class="seat"></div>
-        <small>空位</small>
-      </li>
-      <li>
-        <div class="seat selected"></div>
-
-        <small>選擇</small>
-
-
+        <small style = "color : black">空位</small>
       </li>
       <li>
         <div class="seat occupied"></div>
-        <small>已滿</small>
+        <small style = "color : black">已滿</small>
       </li>
     </ul>
 </div>
@@ -238,10 +226,10 @@
 
       <div class="three"> 
         <div class="row">
-        <div class="seat selected"><h6>6</h6></div>
+        <div class="seat"><h6>6</h6></div>
         <div class="seat"><h6>7</h6></div>
         <div class="seat"><h6>8</h6></div>
-        <div class="seat selected"><h6>9</h6></div>
+        <div class="seat"><h6>9</h6></div>
         <div class="seat"><h6>10</h6></div>
        
       </div></div>
