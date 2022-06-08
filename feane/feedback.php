9e57a7c0e500be919cdc216a820e3e7fa890b3d6
Hongggggggggggg <ryan900911@gmail.com>
2022-06-08 13:54:15 +0800

    v13.1

--- feane/feedback.php
+++ feane/feedback.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$link=mysqli_connect("localhost","root","12345678","sa");
+$link=mysqli_connect("localhost","root","","sa");
 if(isset($_GET["order_id"])){
   $orderid=$_GET["order_id"];
 }
--- feane/manage.php
+++ feane/manage.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$link=mysqli_connect("localhost","root","12345678","sa");
+$link=mysqli_connect("localhost","root","","sa");
 $email=$_SESSION["member_email"];
 
 if(isset($_POST["note"])){
