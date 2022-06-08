0e2eae168a31f4f9b37aee2a9d7923d5dbba09c9
dihung <dihung921@gmail.com>
2022-06-08 22:14:52 +0800

    購物車加減

--- feane/up.php
+++ feane/up.php
@@ -1,12 +1,11 @@
 <?php
 session_start();
-$link=mysqli_connect("localhost","root","","sa");
+require_once("conn.php");
 $email=$_SESSION["member_email"];
 
 if(isset($_GET["meal_id"])){
     $meal_id=$_GET["meal_id"];
-    $sql="update cart set amount = amount + 1 where meal_id = '$meal_id' and email = '$email'";
-    $rs=mysqli_query($link,$sql);
+    $rs = $conn->query("update cart set amount = amount + 1 where meal_id = '$meal_id' and email = '$email'");
      
 }
 
