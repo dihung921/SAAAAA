3d29cbb8a89c2df16fa831b8fcf48d99f509280e
kefangggg <kefang0801@gmail.com>
2022-06-13 17:57:52 +0800

    修

--- feane/down.php
+++ feane/down.php
@@ -10,8 +10,10 @@ if(isset($_GET["meal_id"])){
     $rs1 = $conn->query("select * from `cart` where meal_id = '$meal_id' and email = '$email' ");
     $row=mysqli_fetch_array($rs1);
     $amount=$row["amount"];
+    $price = $row["price"];
     if($amount > 1){
         $rs=$conn->query("update `cart` set amount = amount - 1 where meal_id = '$meal_id' and email = '$email'");
+
         header("Location: cart.php");
     }
 
